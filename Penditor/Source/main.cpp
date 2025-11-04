9c689497d33499dea06d32afa57ba1332d9e0c5f
Malo <mal.sadoine@gmail.com>
2025-11-04 21:39:19 +0100

    [feat] : Bugged Cmake, push to change computer

---
+++ Penditor/Source/main.cpp
@@ -0,0 +1,11 @@
+#include "Angle/Radian.h"
+#include "PenditorHeader.h"
+#include "PengineHeader.h"
+
+#include <stdio.h>
+
+int main () 
+{
+	printf("Helo World !\n");
+	return 0;
+}
---
+++ Pengine/Source/PengineSource.cpp
@@ -0,0 +1,7 @@
+#include "glad/glad.h"
+
+#include "GLFW/glfw3native.h"
+
+#include "PengineDependenciesHeader.h"
+
+#include "PengineHeader.h"
