b97ffc75620c54550678393a7ca46a6ae8c81f59
Malo <mal.sadoine@gmail.com>
2025-11-17 15:55:30 -0500

    [feat] : Create the beginning of the Tranform Component

--- Pengine/Header/PenComponents/PenTransform/PenTransform.h
+++ Pengine/Header/PenComponents/PenTransform/PenTransform.h
@@ -1,10 +1,33 @@
 #pragma once 
 
+#include "PenComponents/PenComponentBase.h"
+#include "Transform.h"
 
 namespace Pengine::Components
 {
-	class PenTransform
+	class PenTransform : public PenComponentBase
 	{
-		
+	public:
+		///* CONSTRUCTOR/DESTRUCTOR */
+		PenTransform(void);
+		PenTransform(const PenTransform& other) = default;
+		PenTransform(PenTransform&& other) = default;
+		~PenTransform(void) = default;
+
+		PenTransform& operator=(const PenTransform& rhs) = default;
+		PenTransform& operator=(PenTransform&& rhs) = default;
+
+		const PenMath::Transform& getGlobalTransform() const;
+		PenMath::Transform& getGlobalTransform();
+
+		const PenMath::Transform& getLocalTransform() const;
+		PenMath::Transform& getLocalTransform();
+
+		void					SetLocalTransform(const PenMath::Transform& transform);
+		void					SetGlobalTransform(const PenMath::Transform& transform);
+
+	private:
+		PenMath::Transform m_globalTransform;
+		PenMath::Transform m_localTransform;
 	};
 }
--- Pengine/Source/PenComponents/PenTransform/PenTransform.cpp
+++ Pengine/Source/PenComponents/PenTransform/PenTransform.cpp
@@ -2,3 +2,46 @@
 
 using namespace Pengine::Components;
 
+PenTransform::PenTransform()
+{
+	this->SetState(PenComponentState::ENABLE, true);
+}
+
+#pragma region Getter
+const PenMath::Transform& PenTransform::getGlobalTransform() const
+{
+	return this->m_globalTransform;
+}
+
+PenMath::Transform& PenTransform::getGlobalTransform()
+{
+	return this->m_globalTransform;
+}
+
+const PenMath::Transform& PenTransform::getLocalTransform() const
+{
+	return this->m_localTransform;
+}
+
+PenMath::Transform& PenTransform::getLocalTransform()
+{
+	return this->m_localTransform;
+}
+void PenTransform::SetLocalTransform(const PenMath::Transform& transform)
+{
+	if (transform == this->m_localTransform)
+		return;
+
+	this->m_localTransform = transform;
+	this->SetState(PenComponentState::DIRTY);
+}
+void PenTransform::SetGlobalTransform(const PenMath::Transform& transform)
+{
+	if (transform == this->m_globalTransform)
+		return;
+
+	this->m_globalTransform = transform;
+	this->SetState(PenComponentState::DIRTY);
+}
+#pragma endregion
+
